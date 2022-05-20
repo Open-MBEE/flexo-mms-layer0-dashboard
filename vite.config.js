dc07b612bdf1458fc7c83b39f0a68b5ff9f9bbc1
Blake Regalia <blake.d.regalia@jpl.nasa.gov>
2022-05-19 17:36:52 -0700

    dev: built-in proxy

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,70 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
-export default defineConfig({
-	plugins: [
-		svelte(),
-	],
-	define: {
-		'global': {},
-	},
-	build: {
-		target: 'chrome90',
-		sourcemap: true,
-		emptyOutDir: false,
-		rollupOptions: {
-			plugins: [
-				rollupNodePolyFill(),
-			],
+export default defineConfig(({mode}) => {
+	const h_env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [
+			svelte(),
+		],
+		define: {
+			'global': {},
 		},
-	},
-	resolve: {
-		alias: {
-			'#': path.resolve(__dirname, './src'),
-			stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-		},
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			define: {
-				global: 'globalThis',
+		resolve: {
+			alias: {
+				'#': path.resolve(__dirname, './src'),
+				stream: 'rollup-plugin-node-polyfills/polyfills/stream',
 			},
-			plugins: [
-				NodeGlobalsPolyfillPlugin({
-					process: true,
-					buffer: true,
-				}),
-				NodeModulesPolyfillPlugin(),
-			],
 		},
-	},
+		optimizeDeps: {
+			esbuildOptions: {
+				define: {
+					global: 'globalThis',
+				},
+				plugins: [
+					NodeGlobalsPolyfillPlugin({
+						process: true,
+						buffer: true,
+					}),
+					NodeModulesPolyfillPlugin(),
+				],
+			},
+		},
+		build: {
+			target: 'chrome90',
+			sourcemap: true,
+			emptyOutDir: false,
+			rollupOptions: {
+				plugins: [
+					rollupNodePolyFill(),
+				],
+			},
+		},
+		server: {
+			proxy: {
+				'^/sparql': {
+					target: h_env.VITE_SPARQL_ENDPOINT,
+					ignorePath: true,
+					changeOrigin: true,
+					autoRewrite: true,
+				},
+				'^/update': {
+					target: h_env.VITE_SPARQ_UPDATE_ENDPOINT,
+					ignorePath: true,
+					changeOrigin: true,
+					autoRewrite: true,
+				},
+				'^/gsp': {
+					target: h_env.VITE_SPARQL_GSP_ENDPOINT,
+					ignorePath: true,
+					changeOrigin: true,
+					autoRewrite: true,
+				},
+			},
+		},
+	};
 });
